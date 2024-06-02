8f7e88c65e0d8b775703aec2e0cecc7baa1b37a0
Luboš Luňák <l.lunak@centrum.cz>
2024-06-02 16:19:38 +0200

    wording tweak

--- Source/Strings.cs
+++ Source/Strings.cs
@@ -4,6 +4,6 @@ namespace STRINGS
     {
         public static LocString NAME = "Bleach Stone Pack";
         public static LocString DESC = "Bleach stone packaged for safe transport";
-        public static LocString RECIPEDESC = "Package containing bleach stone that prevents it from turning into chlorine.";
+        public static LocString RECIPEDESC = "Package containing bleach stone that prevents the bleach stone from turning into chlorine.";
     }
 }
