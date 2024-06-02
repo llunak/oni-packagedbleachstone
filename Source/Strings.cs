7a297020586db130cd20f14be0f8c62cd88c4dd2
Luboš Luňák <l.lunak@centrum.cz>
2024-06-02 16:18:45 +0200

    rename to Bleach Stone Pack
    Shorter, so it fits e.g. the resources list better, and it's
    alphabetically right after bleach stone.

--- Source/Strings.cs
+++ Source/Strings.cs
@@ -2,7 +2,7 @@ namespace STRINGS
 {
     public class PACKAGEDBLEACHSTONE
     {
-        public static LocString NAME = "Packaged Bleach Stone";
+        public static LocString NAME = "Bleach Stone Pack";
         public static LocString DESC = "Bleach stone packaged for safe transport";
         public static LocString RECIPEDESC = "Package containing bleach stone that prevents it from turning into chlorine.";
     }
