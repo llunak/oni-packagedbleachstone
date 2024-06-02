0406e71030c2a56c0a6bf0f319543f67a961a532
Luboš Luňák <l.lunak@centrum.cz>
2024-06-02 15:32:36 +0200

    add the packaged bleach stone item

---
+++ Source/PackagedBleachStone.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace PackagedBleachStone
+{
+    public class PackagedBleachStoneConfig : IEntityConfig
+    {
+        public const string ID = "llunak.PackagedBleachStone";
+
+        public const float PackageSize = 10; // 10kg in one item
+
+        public static ComplexRecipe recipe;
+
+        public string[] GetDlcIds()
+        {
+            return DlcManager.AVAILABLE_ALL_VERSIONS;
+        }
+
+        public GameObject CreatePrefab()
+        {
+            GameObject template = EntityTemplates.CreateLooseEntity(ID, STRINGS.PACKAGEDBLEACHSTONE.NAME,
+                STRINGS.PACKAGEDBLEACHSTONE.DESC, 1f, unitMass: true, Assets.GetAnim("llunak_packagedbleachstone_kanim"),
+                "object", Grid.SceneLayer.Front, EntityTemplates.CollisionShape.RECTANGLE, 0.8f, 0.4f, isPickupable: true);
+            ComplexRecipe.RecipeElement[] array = new ComplexRecipe.RecipeElement[1]
+            {
+                new ComplexRecipe.RecipeElement(SimHashes.BleachStone.CreateTag(), PackageSize)
+            };
+            ComplexRecipe.RecipeElement[] array2 = new ComplexRecipe.RecipeElement[1]
+            {
+                new ComplexRecipe.RecipeElement(ID, 1f)
+            };
+            string text = "Apothecary";
+            recipe = new ComplexRecipe(ComplexRecipeManager.MakeRecipeID(text, array, array2), array, array2)
+            {
+                time = 20f,
+                description = STRINGS.PACKAGEDBLEACHSTONE.RECIPEDESC,
+                nameDisplay = ComplexRecipe.RecipeNameDisplay.Result,
+                fabricators = new List<Tag> { text },
+                sortOrder = 50,
+                requiredTech = "MedicineII"
+            };
+            template.AddOrGet<EntitySplitter>();
+            template.AddTag(GameTags.ConsumableOre);
+            template.AddTag(ID.ToTag()); // For Storage to find it by tag.
+            return template;
+        }
+
+        public void OnPrefabInit(GameObject inst)
+        {
+        }
+
+        public void OnSpawn(GameObject inst)
+        {
+        }
+    }
+}
---
+++ Source/Strings.cs
@@ -0,0 +1,9 @@
+namespace STRINGS
+{
+    public class PACKAGEDBLEACHSTONE
+    {
+        public static LocString NAME = "Packaged Bleach Stone";
+        public static LocString DESC = "Bleach stone packaged for safe transport";
+        public static LocString RECIPEDESC = "Package containing bleach stone that prevents it from turning into chlorine.";
+    }
+}
